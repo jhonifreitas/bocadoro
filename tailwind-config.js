a13288598d5f283444d9b0ee42d0a94ac382a78e
rosalvo <rosalvo_freitas@hotmail.com>
2023-03-14 17:01:39 -0300

    cabeçalho e banner

---
+++ tailwind-config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true, 
+        padding: '1rem'
+      },
+      borderRadius: {
+        circle: '50%',
+      },
+      colors: {
+        primary: '#9A0310',
+        'primary-dark': '#88030e',
+        secondary: '#0C6C00',
+        'secondary-dark': '#084600',
+      }
+    }
+  }
+}
